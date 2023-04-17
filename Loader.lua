8cc96d4102208a879815491181d73af493297925
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2023-04-16 19:17:26 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,17 @@
 
+repeat
+    task.wait()
+until game:IsLoaded()
+
+
+getgenv().games = {
+    4483381587
+}
+
+for i,v in pairs(getgenv().games) do
+    if v == game.PlaceId then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FearLLC/FearV1/main/Games/"..game.PlaceId..".lua", true))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FearLLC/FearV1/main/Universal.lua", true))()
+    end
+end
